9ed911196dbc16021ba81d6ee14d1e37c1df34a8
LORENA DA SILVA SANTOS <135022344+rizinho@users.noreply.github.com>
2024-06-03 11:45:56 -0300

    aula-03/06

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
-  return (
+  return(
     <div>
-      <h1> Home </h1>
+      <h1>projeto</h1>
+
+      <Link href="/home">BUtttt</Link>
     </div>
   );
 }
---
+++ src/app/temp/page.jsx
@@ -0,0 +1,19 @@
+import styles from "./page.module.css"
+import Link from "next/link";
+
+export default function Home() {
+    return(
+        <div className="container">
+        <h1>Lista Teste</h1>
+        <Link href="/home"               > <p className={styles.linkTemp}>Inicio</p>              </Link>
+        <Link href="/home/liMedico"      > <p className={styles.linkTemp}>Lista de medicos</p>    </Link>
+        <Link href="/home/liPaciente"    > <p className={styles.linkTemp}>Lista pacientes</p>     </Link>
+        <Link href="/home/telAgen"       > <p className={styles.linkTemp}>Tela de agendamento</p> </Link>
+        <Link href="/home/telMedico"     > <p className={styles.linkTemp}>Tela de medico</p>      </Link>
+        <Link href="/home/telMensa"      > <p className={styles.linkTemp}>Tela de Mensagens</p>   </Link>
+        <Link href="/usuarios/cadCliente"> <p className={styles.linkTemp}>Cadastro do cliente</p> </Link>
+        <Link href="/usuarios/cadMedico" > <p className={styles.linkTemp}>Cadastro do medico</p>  </Link>
+        <Link href="/usuarios/login"     > <p className={styles.linkTemp}>Login</p>               </Link>
+        </div>
+    )
+}
