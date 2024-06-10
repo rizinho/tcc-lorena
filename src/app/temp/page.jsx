ff9eab7b622177b0f362b3fd5ad918b4bc371f8e
LORENA DA SILVA SANTOS <135022344+rizinho@users.noreply.github.com>
2024-06-10 09:46:51 -0300

    aula10-06

--- src/app/temp/page.jsx
+++ src/app/temp/page.jsx
@@ -11,8 +11,8 @@ export default function Home() {
         <Link href="/home/telAgen"       > <p className={styles.linkTemp}>Tela de agendamento</p> </Link>
         <Link href="/home/telMedico"     > <p className={styles.linkTemp}>Tela de medico</p>      </Link>
         <Link href="/home/telMensa"      > <p className={styles.linkTemp}>Tela de Mensagens</p>   </Link>
-        <Link href="/usuarios/cadCliente"> <p className={styles.linkTemp}>Cadastro do cliente</p> </Link>
-        <Link href="/usuarios/cadMedico" > <p className={styles.linkTemp}>Cadastro do medico</p>  </Link>
+        <Link href="/usuarios/cadcliente"> <p className={styles.linkTemp}>Cadastro do cliente</p> </Link>
+        <Link href="/usuarios/cadmedico" > <p className={styles.linkTemp}>Cadastro do medico</p>  </Link>
         <Link href="/usuarios/login"     > <p className={styles.linkTemp}>Login</p>               </Link>
         </div>
     )
